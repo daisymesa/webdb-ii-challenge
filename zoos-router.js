4ee6fbd9fbfafeb7d05e3aceb667f0a79b48018d
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-06-19 16:41:26 -0400

    Adding delete request

--- zoos-router.js
+++ zoos-router.js
@@ -52,21 +52,39 @@ router.post('/', (req, res) => {
 // CREATE
 router.put('/:id', (req, res) => {
     const changes = req.body;
-    const id = req.params.id;
+    const { id } = req.params;
+    
     db('zoos')
-    .where({ id: id })
-    .update(changes)
-    .then(count => {
-        if(count) {
-            res.status(200).json({ count })
-        } else {
-            res.status(404).json({ error: 'Zoo ID not found'})
-        }
-    })
-    .catch(error => {
-        res.status(500).json({ error: error })
-    })
+        .where({ id: id })
+        .update(changes)
+        .then(count => {
+            if (count) {
+                res.status(200).json(count)
+            } else {
+                res.status(404).json({ error: 'Zoo ID not found' })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: error })
+        })
 })
 
+// DELETE
+router.delete('/:id/', (req, res) => {
+    const { id } = req.params;
 
+    db('zoos')
+        .where({ id: id })
+        .del()
+        .then(count => {
+            if (count) {
+                res.json(count);
+            } else {
+                res.status(404).json({ message: 'The Zoo with the specified ID does not exist.' })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: error })
+        })
+})
 module.exports = router;
